788029415827b257c71440ab301f888770c1b784
Tiffany Tang <tifftang22@gmail.com>
2019-10-11 18:10:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
-      {:name=> "Alan Anderson",
+      {:name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
         :points => 22,
